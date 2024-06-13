a59e08bf3c134b02d65fab35a5b5aa4d45855328
Adilson Gustavo <adilsons2tv@gmail.com>
2024-06-12 22:29:34 -0300

    teste botoes pyautogui

---
+++ botoes.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+pyautogui.click(1065,166, duration=2)
+pyautogui.hotkey('ctrl','a')
+pyautogui.hotkey('ctrl','c')
+pyautogui.click(1161,718, duration=1)
+pyautogui.hotkey('ctrl','v')
